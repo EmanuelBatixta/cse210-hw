e6fe2117f91b48a137bb16e307c4ef304a4935be
EmanuelBatixta <emanueljonatas28@gmail.com>
2024-09-21 10:49:21 -0300

    correct the bug of letters

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,47 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        
         DisplayWelcomeMessage();
 
-        displayResult();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int square = SquareNumber(num);
 
-        
-        static void DisplayWelcomeMessage()
-        {
-            Console.WriteLine("Welcome to the program!");
-        }
+        DisplayResult(name, square);
+    }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
 
-        static string PromptUserName()
-        {
-            Console.Write("Please, enter your name: ");
-            string name = Console.ReadLine();
-            return name;
-        }
+    static string PromptUserName()
+    {
+        Console.Write("Please, enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
 
-        static int PromptUserNumber()
-        {
-            Console.Write("Please enter your favorite number: ");
-            string num = Console.ReadLine();
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string num = Console.ReadLine();
 
-            int iNum = int.Parse(num);
-            return iNum;
-        }
+        int iNum = int.Parse(num);
+        return iNum;
+    }
 
-        static int SquareNumber(int iNum)
-        {
-            int square = iNum*iNum;
-            return square;
-        }
+    static int SquareNumber(int iNum)
+    {
+        int square = iNum*iNum;
+        return square;
+    }
 
-        static void DisplayResult()
-        {
-            string name = promptUserName();
-            int num = promptUserNumber();
-            int square = squareNumber(num);
-
-            Console.WriteLine($"{name}, the square of your number is {square}");
-        }
-
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
