08f8de7ee60ff7efead66baaa2f0684eb0f20f7c
EmanuelBatixta <emanueljonatas28@gmail.com>
2024-10-18 19:02:50 -0300

    update prepartion material

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        MathAssignment _mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        WritingAssignment _writingAssignment = new WritingAssignment("Samuel Bennett", "European History", "The Causes of World War II");
+
+        Console.WriteLine(_writingAssignment.GetSummary());
+        Console.WriteLine(_writingAssignment.GetWritingInformation());
+
+        Console.WriteLine(_mathAssignment.GetSummary());
+        Console.WriteLine(_mathAssignment.GetHomeworkList());
     }
 }
