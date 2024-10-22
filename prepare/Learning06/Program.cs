c80a35a971d6be573c1e8e8a4edb6af49d3e90bd
EmanuelBatixta <emanueljonatas28@gmail.com>
2024-10-21 22:10:06 -0300

    prepare learning week 6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+
+        Console.Clear();
+
+        Circle circle = new Circle("yellow",7);
+        Rectangle rectangle = new Rectangle("cyan",8,5);
+        Square square = new Square("red",4);
+
+        List<Shapes> _shapes = new List<Shapes>{
+            circle,
+            rectangle,
+            square
+        };
+
+        foreach(Shapes s in _shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
